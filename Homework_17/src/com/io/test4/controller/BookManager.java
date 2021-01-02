132bf3de9993b490828f96f020251b15f2a37cfa
Yujin Jeon <75886211+YujinJeon416@users.noreply.github.com>
2021-01-02 20:46:07 +0900

    Update BookManager.java

--- Homework_17/src/com/io/test4/controller/BookManager.java
+++ Homework_17/src/com/io/test4/controller/BookManager.java
@@ -43,34 +43,34 @@ public class BookManager implements Serializable{
 		}
 		
 		public void fileSave() {
-			Book[] book = new Book[] {
+			Book[] book = new Book[] {  // Book 객체 배열 선언, 5개 초기화함 //샘플데이터 임의 작성
 					new Book("코스모스","칼 세이건",16000, new GregorianCalendar(2006,12,20),0.06), 
 					new Book("골든 아워", "이국종", 14000, new GregorianCalendar(2018,10,02),0.1),
 					new Book("이기적 유전자","",18000, new GregorianCalendar(2018,10,20),0.1),
 					new Book("우울할 땐 뇌과학","알렉스 코브", 15000, new GregorianCalendar(2018,03,12),0.1),
 					new Book("여행의 이유", "김영하", 12000, new GregorianCalendar(2019,04,17),0.05)
 			};
-			try(ObjectOutputStream oos = new 
-					ObjectOutputStream(new FileOutputStream("books.dat"));){
+			try(ObjectOutputStream oos = new   //try with resource 문 사용할 것
+					ObjectOutputStream(new FileOutputStream("books.dat"));){  // "books.dat" 파일에 객체 기록 저장함
 				oos.writeObject(book);
 			}catch(IOException e) {
 				e.printStackTrace();
 				
 			}
-			System.out.println("books.dat에 저장 완료!");
+			System.out.println("books.dat에 저장 완료!"); //"books.dat 에 저장 완료!" 출력
 			
 		}
 		
 		public void fileRead() {
-			Book[] book = new Book[10];
-			try(ObjectInputStream ois = 
-					new ObjectInputStream(new FileInputStream("books.dat"));) {
+			Book[] book = new Book[10];   // Book 객체 배열 선언 : 10개
+			try(ObjectInputStream ois =     //try with resource 문 사용할 것
+					new ObjectInputStream(new FileInputStream("books.dat"));) {  // "books.dat" 파일에서 데이터 읽어서 배열에 저장함
 				book = (Book[])ois.readObject();
 				for(int i = 0; i < book.length; i++) {
-					System.out.println(book[i]);
+					System.out.println(book[i]);  //객체 정보를 화면에 출력함
 				}
 			}catch(EOFException | ClassNotFoundException e) {
-				System.out.println("books.dat 읽기 완료!");
+				System.out.println("books.dat 읽기 완료!");   //"books.dat 읽기 완료!" 출력
 			}catch(IOException e) {
 				e.printStackTrace();
 			}
