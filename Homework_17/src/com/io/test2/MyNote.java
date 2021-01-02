01db528c47fc6f11983e9ae2e14e94f08e8095ae
Yujin Jeon <75886211+YujinJeon416@users.noreply.github.com>
2021-01-02 20:31:54 +0900

    Update MyNote.java

--- Homework_17/src/com/io/test2/MyNote.java
+++ Homework_17/src/com/io/test2/MyNote.java
@@ -55,37 +55,37 @@ public class MyNote {
 
 	  
 
-	    public void fileSave() {
-	        StringBuilder sb = new StringBuilder();
+	    public void fileSave() {    //* fileSave() 메소드
+	        StringBuilder sb = new StringBuilder();           //StringBuilder 를 사용하여 문자열을 반복해서 입력받아 추가함
 	        String content = null;
-	        System.out.println("파일에 저장할 내용을 입력하시오(\\\"exit\"\\을 입력하면 내용 입력 끝) ");
+	        System.out.println("파일에 저장할 내용을 입력하시오(\\\"exit\"\\을 입력하면 내용 입력 끝) "); // "파일에 저장할 내용을 입력하시오"  출력, 
 	        while (true) {
 	            content = sc.nextLine();
 
-	            if (content.equalsIgnoreCase("exit")) break;
+	            if (content.equalsIgnoreCase("exit")) break;               //"exit" 입력되면 반복 종료함
 	            else {
 	                sb.append(content);
 	                sb.append("\n");
 	            }
 	        }
 
-	        System.out.print("저장하시겠습니까? (y/n) : ");
+	        System.out.print("저장하시겠습니까? (y/n) : "); // "저장하시겠습니까? (y/n) : " 출력
 //	        String save =
 
 	        if (sc.next().equalsIgnoreCase("y")) {
 	            BufferedWriter bw = null;
 	            try {
 	                sc.nextLine();
-	                System.out.print("저장할 파일명 : ");
+	                System.out.print("저장할 파일명 : "); // 입력값이 대소문자 상관없이 'Y' 이면, "저장할 파일명 : " 출력
 	                String title = sc.nextLine();
-	                bw = new BufferedWriter(new FileWriter(title + ".txt"));
+	                bw = new BufferedWriter(new FileWriter(title + ".txt")); //파일명 입력받아, 해당 파일에 출력 기록함.
 	                bw.write(sb.toString());
-	                System.out.println("입력받은 파일명.txt 파일에 성공적으로 저장하였습니다.");
+	                System.out.println("입력받은 파일명.txt 파일에 성공적으로 저장하였습니다."); //"oooo.txt 파일에 성공적으로 저장하였습니다." 출력
 	            } catch (IOException e) {
 	                e.printStackTrace();
-	            } finally {
+	            } finally {                            
 	                try {
-	                    bw.close();
+	                    bw.close();                        //스트림 반납하고 리턴함
 	                } catch (IOException e) {
 	                    e.printStackTrace();
 	                }
@@ -99,24 +99,24 @@ public class MyNote {
 	    }
 
 	    public void fileOpen() {
-	        System.out.println("열기 할 파일명 : ");
-	        String title = sc.nextLine();
-	        BufferedReader br = null;
+	        System.out.println("열기 할 파일명 : ");  // "열기할 파일명 : " 출력하고, 파일명 입력받음
+	        String title = sc.nextLine();                     
+	        BufferedReader br = null;                      
 	        String content = null;
 	        try {
 
-	            br = new BufferedReader(new FileReader(title + ".txt"));
-	            while ((content = br.readLine()) != null) {
+	            br = new BufferedReader(new FileReader(title + ".txt"));      // //파일 입력 스트림 생성
+	            while ((content = br.readLine()) != null) {         //파일의 내용을 한 줄씩 읽어서 화면에 출력함
 	                System.out.println(content);
 	            }
 
-	        } catch (FileNotFoundException e) {
+	        } catch (FileNotFoundException e) {          
 	            e.printStackTrace();
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        } finally {
 	            try {
-	                br.close();
+	                br.close();                //스트림 반납하고 리턴
 	            } catch (IOException e) {
 	                e.printStackTrace();
 	            }
@@ -125,24 +125,24 @@ public class MyNote {
 
 	    }
 
-	    public void fileEdit() {
+	    public void fileAppend() {
 	        BufferedReader br = null;
 	        BufferedWriter bw = null;
-	        System.out.print("수정 할 파일명 : ");
+	        System.out.print("수정 할 파일명 : ");        //"수정할 파일명 : " 출력하고, 파일명 입력받음
 	        String title = sc.next();
 	        sc.nextLine();
 	        String content;
 	        String add;
-	        StringBuilder sb = new StringBuilder();
+	        StringBuilder sb = new StringBuilder();        
 
 	        try {
-	            br = new BufferedReader(new FileReader(title + ".txt"));
-	            bw = new BufferedWriter(new FileWriter(title + ".txt",true));
+	            br = new BufferedReader(new FileReader(title + ".txt"));    //파일 입력 스트림 생성
+	            bw = new BufferedWriter(new FileWriter(title + ".txt",true));  //파일 출력 스트림 생성 : 단, 기존 내용 둔 상태로 생성할 것
 	            while((content = br.readLine())!=null){
-	                System.out.println(content);
+	                System.out.println(content);          //파일의 내용을 읽어서 화면출력.     
 	            }
-	            System.out.print("파일에 추가할 내용을 입력하시오(\\\"exit\\\"을 입력하면 내용 입력 끝) : ");
-
+	            System.out.print("파일에 추가할 내용을 입력하시오(\\\"exit\\\"을 입력하면 내용 입력 끝) : ");   // "파일에 추가할 내용을 입력하시오." 출력하고, 추가할 내용을 StringBuilder에 저장하기
+                                                                                                                 // "exit" 입력하면 반복 종료함
 	            while(true){
 	                add = sc.nextLine();
 
@@ -154,19 +154,19 @@ public class MyNote {
 	                }
 
 	            }
-	            System.out.print("변경된 내용을 저장하시겠습니까?(y/n)");
+	            System.out.print("변경된 내용을 저장하시겠습니까?(y/n)");        //"저장하시겠습니까? (y/n) : " 출력
 
-	            String save = sc.nextLine();
+	            String save = sc.nextLine();                                   // 입력값이 대소문자 상관없이 'Y' 이면, 해당 파일에 출력 기록함.
 	            if(save.equalsIgnoreCase("y")){
 	                bw.write(sb.toString());
-	                System.out.println(title + ".txt 파일의 내용이 변경되었습니다.");
+	                System.out.println(title + ".txt 파일의 내용이 변경되었습니다.");  // "ooo.txt 파일의 내용이 변경되었습니다." 출력
 	            }
 	        } catch (FileNotFoundException e) {
 	            e.printStackTrace();
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        } finally {
-	            try{
+	            try{                             // 스트림 반납하고 리턴함
 	                br.close();
 	                bw.close();
 	            } catch (IOException e) {
