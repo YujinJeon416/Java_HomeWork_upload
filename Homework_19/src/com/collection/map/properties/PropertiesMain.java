fa564ab818365dc280fc75761c60d5f299686441
Yujin Jeon <75886211+YujinJeon416@users.noreply.github.com>
2021-01-06 15:57:14 +0900

    Properties 실습문제

--- Homework_19/src/com/collection/map/properties/PropertiesMain.java
+++ Homework_19/src/com/collection/map/properties/PropertiesMain.java
@@ -12,35 +12,40 @@ com.collection.map.properties.PropertiesMain.java
  * 
  *
  */
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Properties;
+
+import java.io.FileNotFoundException;
 
 public class PropertiesMain {
-	
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+		new PropertiesMain().start();
+	}
+
+	private void start() {
 		Properties prop = new Properties();
 		
 		try {
-			prop.load(new FileInputStream("resources/config.Properties"));
+			prop.load(new FileReader("resources/config.properties"));
+			
+//			prop.list(System.out);
+			
+			String OS = prop.getProperty("OS");
+			String Price = prop.getProperty("Price");
+			String Company = prop.getProperty("Company");
+			String PrimaryUI = prop.getProperty("PrimaryUI");
+			
+			System.out.println("OS="+OS);
+			System.out.println("Price="+Price);
+			System.out.println("Company="+Company);
+			System.out.println("PrimaryUI="+PrimaryUI);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		
-		
-		
-		Enumeration<?> names = prop.propertyNames();	// key 모음  - 이뉴머레이션 활용
-														// iterator 같은 기능
-		while(names.hasMoreElements()) {
-			String name = (String)names.nextElement();	// name은 key만 모아놨음
-			
-			String value = prop.getProperty(name);
-			System.out.println(name + " = " + name);
 		}
 	}
 
 }
+
 
 
